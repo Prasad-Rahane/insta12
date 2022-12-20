d0ec4dd7b7d7456e06a72b2e091786dbaea0763b
Prasad-Rahane <71807238+Prasad-Rahane@users.noreply.github.com>
2022-12-20 14:03:33 +0530

    Add files via upload

---
+++ client/build/precache-manifest.ef65edf4e11ac555e90df0adb3156f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a66ae9c21f48a28456",
+    "url": "/static/css/main.8741ef89.chunk.css"
+  },
+  {
+    "revision": "90a66ae9c21f48a28456",
+    "url": "/static/js/main.c6e5fab3.chunk.js"
+  },
+  {
+    "revision": "151f7dc7931251360b3e",
+    "url": "/static/js/runtime~main.f5d8407b.js"
+  },
+  {
+    "revision": "c0bf079091a545aa07c1",
+    "url": "/static/js/2.bb5ac2b1.chunk.js"
+  },
+  {
+    "revision": "60cf4ecf76dd825d6b0e",
+    "url": "/static/js/3.b84871a5.chunk.js"
+  },
+  {
+    "revision": "77f78253b14af0524e8b861c5cf0ee09",
+    "url": "/index.html"
+  }
+];
